b0d243575a3b1e3639e9d92c7359984e0d921b07
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-14 17:46:45 +0900

    add client

---
+++ client/client.py
@@ -0,0 +1,11 @@
+
+class CHECK():
+    def __init__(self):
+        print("Start")
+
+    def checkPrint(self):
+        print("*")
+
+if __name__=="__main__":
+    check=CHECK()
+    check.checkPrint
