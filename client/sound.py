bdef1bbb6f3a989c89fa672f7000734f6a70aec5
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 16:18:12 +0900

    Update sound.py

--- client/sound.py
+++ client/sound.py
@@ -11,6 +11,7 @@ class SOUND:
             tts = gTTS(text=text, lang=lang, slow=False)  # 텍스트를 TTS 객체로 변환
             filename = "station.wav"  # 임시 오디오 파일 이름
             tts.save(filename)  # 오디오 파일로 저장
+            pygame.mixer.init()
             sound = pygame.mixer.Sound(filename)
             sound.play()
             #os.remove(filename)  # 재생 후 오디오 파일 삭제
