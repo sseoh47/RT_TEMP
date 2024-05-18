4f6f13b49143520dfb0cead80f3e8ba41b88b89b
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 22:23:45 +0900

    Update test.py

--- client/test.py
+++ client/test.py
@@ -1,9 +1,7 @@
-# from gtts import gTTS
 import time
 import pygame    
 
-
-def text_to_speech(self):
+def text_to_speech():
         try:
             # tts = gTTS(text=text, lang=lang, slow=False)  # 텍스트를 TTS 객체로 변환
             filename = "./station.wav"  # 임시 오디오 파일 이름
@@ -13,9 +11,10 @@ def text_to_speech(self):
             print("*")
             pygame.mixer.music.load(filename)
             pygame.mixer.music.play()
-            self.play_wav_file(filename)
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
             # os.remove(filename)  # 재생 후 오디오 파일 삭제
         except Exception as e:
-            print(f"Error in text_to_speech_pyaudio: {e}")
+            print(f"Error in text_to_speech: {e}")
 
 text_to_speech()
