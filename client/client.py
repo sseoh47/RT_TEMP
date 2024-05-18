775d49cab2efdecbdb4a4b73acd26fc4408e1c20
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 21:11:20 +0900

    Update client.py

--- client/client.py
+++ client/client.py
@@ -10,12 +10,10 @@ from sound import*
 class Client():
     def __init__(self):
         self.running = True
-        self.host = HOST  # 서버의 IP 주소
-        self.port = PORT  # 서버의 포트 번호
         # 소켓 생성 및 서버에 연결
         # [OS ERROR 10038] 소켓으로 인한 에러 해결 -> 연결 지속하기에 소켓 하나만 사용하기로
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.host, self.port))
+        self.sock.connect((HOST, PORT))
 
     def send_data_to_server(self, data):
         try:
