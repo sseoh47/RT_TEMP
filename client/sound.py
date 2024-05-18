dd6a03558134f265310c3d012a5f76b49531294b
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 15:51:16 +0900

    Update sound.py

--- client/sound.py
+++ client/sound.py
@@ -9,7 +9,7 @@ class SOUND:
     def text_to_speech(text, lang='ko'):
         try:
             tts = gTTS(text=text, lang=lang, slow=False)  # 텍스트를 TTS 객체로 변환
-            filename = "sample.wav"  # 임시 오디오 파일 이름
+            filename = "station.wav"  # 임시 오디오 파일 이름
             tts.save(filename)  # 오디오 파일로 저장
             playsound(filename)  # 저장된 오디오 파일 재생
             os.remove(filename)  # 재생 후 오디오 파일 삭제
