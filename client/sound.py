cdcb67ff3b309a4b5a7eddcf977ce6fea7d66467
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 22:34:20 +0900

    Update sound.py

--- client/sound.py
+++ client/sound.py
@@ -11,7 +11,7 @@ class SOUND:
     def text_to_speech(self, text, lang='en'):
         try:
             tts = gTTS(text=text, lang=lang, slow=False)  # 텍스트를 TTS 객체로 변환
-            filename = "./station.wav"  # 임시 오디오 파일 이름
+            filename = "./station.mp3"  # 임시 오디오 파일 이름
             tts.save(filename)  # 오디오 파일로 저장
             pygame.init()
             time.sleep(1)
