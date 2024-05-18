ea6ca8a3db13f8b91e969d4aa62c9f22f1dd811f
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 17:04:13 +0900

    Update sound.py

--- client/sound.py
+++ client/sound.py
@@ -15,10 +15,11 @@ class SOUND:
             tts.save(filename)  # 오디오 파일로 저장
             pygame.init()
             time.sleep(1)
-            pygame.mixer.music.load(filename)
-            pygame.mixer.music.play()
+            playsound(filename)
+            # pygame.mixer.music.load(filename)
+            # pygame.mixer.music.play()
             # self.play_wav_file(filename)
-            #os.remove(filename)  # 재생 후 오디오 파일 삭제
+            # os.remove(filename)  # 재생 후 오디오 파일 삭제
         except Exception as e:
             print(f"Error in text_to_speech_pyaudio: {e}")
 
