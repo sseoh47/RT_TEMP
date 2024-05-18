5392640bc726070397e8a29de10f6963ec552bae
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 16:16:00 +0900

    change playsound to pygamemixer

---
+++ client/ps.py
@@ -0,0 +1,3 @@
+# from playsound import playsound
+
+# playsound(filename)  # 저장된 오디오 파일 재생
--- client/sound.py
+++ client/sound.py
@@ -1,5 +1,5 @@
 from gtts import gTTS
-from playsound import playsound
+import pygame
 import os
 
 class SOUND:
@@ -11,7 +11,8 @@ class SOUND:
             tts = gTTS(text=text, lang=lang, slow=False)  # 텍스트를 TTS 객체로 변환
             filename = "station.wav"  # 임시 오디오 파일 이름
             tts.save(filename)  # 오디오 파일로 저장
-            playsound(filename)  # 저장된 오디오 파일 재생
+            sound = pygame.mixer.Sound(filename)
+            sound.play()
             #os.remove(filename)  # 재생 후 오디오 파일 삭제
         except Exception as e:
             print(f"Error in text_to_speech_pyaudio: {e}")
