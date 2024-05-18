d89b24949d3c03ddade460a03d1a573f1f539040
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-18 16:23:31 +0900

    .

--- client/sound.py
+++ client/sound.py
@@ -10,12 +10,11 @@ class SOUND:
     def text_to_speech(self, text, lang='en'):
         try:
             tts = gTTS(text=text, lang=lang, slow=False)  # 텍스트를 TTS 객체로 변환
-            filename = "station.wav"  # 임시 오디오 파일 이름
+            filename = "./station.wav"  # 임시 오디오 파일 이름
             tts.save(filename)  # 오디오 파일로 저장
-            # pygame.mixer.init()
-            # sound = pygame.mixer.Sound(filename)
-            # sound.play()
-            starts()
+            pygame.mixer.init()
+            sound = pygame.mixer.Sound(filename)
+            sound.play()
             #os.remove(filename)  # 재생 후 오디오 파일 삭제
         except Exception as e:
             print(f"Error in text_to_speech_pyaudio: {e}")
