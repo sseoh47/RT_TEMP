3b7ca0d19e1f63e3ac4d8265307b7f0a17f474cc
seohee <108114351+sseoh47@users.noreply.github.com>
2024-06-02 22:47:42 +0900

    Update master_model.py

--- main/model/master_model.py
+++ main/model/master_model.py
@@ -3,8 +3,8 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import requests
 
-# TMAP_APPKEY = "wuLynpCfphaaJZkwoId0w4AWaJb9mbht1IVtIX0K"  # __ 혜림
-TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8" # __ 서희
+TMAP_APPKEY = "wuLynpCfphaaJZkwoId0w4AWaJb9mbht1IVtIX0K"  # __ 혜림
+# TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8" # __ 서희
 
 class Master_Model:
     def __init__(self):
