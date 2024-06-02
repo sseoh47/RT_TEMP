909426f1b0bc4b0afde5708df2565a8334e5404f
김민관 <alsrhks2508@naver.com>
2024-06-02 16:24:18 +0900

    tesat

---
+++ client/speeech_test.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+#import sys #-- 텍스트 저장시 사용
+
+r = sr.Recognizer()
+kr_audio = sr.AudioFile('./test.wav')
+
+with kr_audio as source:
+    audio = r.record(source)
+
+#sys.stdout = open('news_out.txt', 'w') #-- 텍스트 저장시 사용
+print(r.recognize_google(audio, language='ko-KR'))
