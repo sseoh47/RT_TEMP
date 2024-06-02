f9cd8ce880a7a1fd509bcb342e2256fb2c0623bc
seohee <108114351+sseoh47@users.noreply.github.com>
2024-06-02 15:28:35 +0900

    Update main.py

--- client/main/main.py
+++ client/main/main.py
@@ -2,6 +2,7 @@ from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 from queue import Queue
 import time
+from src.network_provider import NetworkProvider
 
 if __name__=="__main__":
     netWorkProvider=NetworkProvider()
