8b7ba81c8315db3bf68246b5bd9f8a2896ef70a2
김민관 <alsrhks2508@naver.com>
2024-06-02 16:27:40 +0900

    Update speeech_test.py

--- client/speeech_test.py
+++ client/speeech_test.py
@@ -8,4 +8,4 @@ with kr_audio as source:
     audio = r.record(source)
 
 #sys.stdout = open('news_out.txt', 'w') #-- 텍스트 저장시 사용
-print(r.recognize_google(audio, language='ko-KR'))
+print(r.recognize_google_cloud(audio, language='ko-KR'))
