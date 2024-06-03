fe74970c73decd06eece30fd786effd030395006
seohee <108114351+sseoh47@users.noreply.github.com>
2024-06-03 19:00:09 +0900

    Update test.py

--- client/test.py
+++ client/test.py
@@ -4,17 +4,13 @@ import pygame
 def text_to_speech():
         try:
             pygame.init()
-            
-            # WAV 파일을 재생하려면 pygame.mixer.Sound 사용
-            sound = pygame.mixer.Sound('./test.wav')
-            sound.play()
-            print("*")
-            
-            # 재생 시간 동안 대기
-            time.sleep(sound.get_length())
-            
-            # pygame 종료
-            pygame.mixer.quit()
+            pygame.mixer.music.load("./test.wav")
+            #play
+            pygame.mixer.music.play()
+            #끝까지 재생할때까지 기다린다.
+            while pygame.mixer.music.get_busy() == True:
+                continue
+
         except Exception as e:
             print(f"Error in text_to_speech: {e}")
 
