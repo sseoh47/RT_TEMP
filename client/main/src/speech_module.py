b02924b7ba547dbd5bbb0fe57eeba50860608fef
hyellm <yhl9779@naver.com>
2024-06-04 01:28:46 +0900

    Update speech_module.py

--- client/main/src/speech_module.py
+++ client/main/src/speech_module.py
@@ -9,6 +9,7 @@ def text_to_speech():
         pygame.time.wait(int(sounda.get_length() * 1000))
 
     except Exception as e:
+        print(e)
         pass
         # print(f"Error in text_to_speech: {e}")
 
