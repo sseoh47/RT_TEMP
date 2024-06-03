7efff900edeadbb257f50b93a999c1b3cd12c67a
seohee <108114351+sseoh47@users.noreply.github.com>
2024-06-03 19:06:32 +0900

    Update test.py

--- client/test.py
+++ client/test.py
@@ -2,16 +2,17 @@ import time
 import pygame    
 
 def text_to_speech():
-        try:
-            pygame.init()
-            pygame.mixer.music.load("./test.wav")
-            #play
-            pygame.mixer.music.play()
-            #끝까지 재생할때까지 기다린다.
-            while pygame.mixer.music.get_busy() == True:
-                continue
+    try:
+        pygame.init()
+        pygame.mixer.music.load("./test.wav")
+        #play
+        pygame.mixer.music.play()
+        print("*")
+        #끝까지 재생할때까지 기다린다.
+        while pygame.mixer.music.get_busy() == True:
+            continue
 
-        except Exception as e:
-            print(f"Error in text_to_speech: {e}")
+    except Exception as e:
+        print(f"Error in text_to_speech: {e}")
 
 text_to_speech()
